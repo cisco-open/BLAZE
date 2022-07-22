a0ee29dae2693c56af6ad3d235214f86ab431af1
Jayanth Srinivasa <jayanth.srinivasa@berkeley.edu>
2022-07-22 17:39:11 +0000

    Revert "completing the fix for #1 and #2" to fix unnecessary deletes
    This reverts commit 7fc8bf581cacd144706b51b32cd21beb92109fc3.

---
+++ Interfaces/Model_interface_Search.py
@@ -0,0 +1,17 @@
+class Model_Search():
+
+    def __init__(self, file_name, file_content):
+        self.file_name = file_name
+        self.file_content = file_content
+
+    def file_search(self, search_term):
+        ''' Load a model from a directory or library'''
+        pass
+
+    def run(self):
+        ''' Run the model'''
+        pass
+
+    def prime_model_to_run(self):
+        '''Index/fine-tune if necessary'''
+        pass
