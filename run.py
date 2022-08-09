778eb7a35aa4de100fb319864d87018c9180f03a
tlemenestrel <tlemenestrel@hotmail.fr>
2022-08-09 03:37:21 -0700

    Fixing merge conflitcs - working prototype

--- run.py
+++ run.py
@@ -1,20 +1,9 @@
 import argparse
-from time import sleep
-import yaml
-#from aski.dash_files.app_callbacks import *
-from aski.flask_servers.app import *
 from multiprocessing import Process
-import os
-import subprocess
+import yaml
 
-
-def run_app_server(app, port=3000, ip='localhost'):
-   # os.environ["WERKZEUG_RUN_MAIN"] = 'true'
-    print("PID:", os.getpid())
-    print("Werkzeug subprocess:", os.environ.get("WERKZEUG_RUN_MAIN"))
-    print("Inherited FD:", os.environ.get("WERKZEUG_SERVER_FD"))
-    app.run(host=ip, port=port)
-
+from aski.dash_files.app_callbacks import run_app
+from aski.flask_servers.app import create_app, run_app_server
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -25,19 +14,15 @@ if __name__ == "__main__":
 
     with open(args.yaml_file, mode="rt", encoding="utf-8") as file:
         data = yaml.safe_load(file)
-   # print(data)
-   # p = Process(target=run_app, args=(data,))
-    with open("tmp.yaml", mode="wt", encoding="utf-8") as file:
-        yaml.dump(data, file)
-    print("Created tmp.yaml")
-    sleep(5)
-    app = create_app(data)
-    p1 = Process(target=run_app_server, args=(app,))
-    # p.start()
-    p1.start()
-    # p.start()
-    subprocess.Popen(['python', 'run_dash.py', 'tmp.yaml'])
-    p1.join()
-    # p.join()
 
-    # os.remove("tmp.yaml")
+    app = create_app(data)
+
+    p = Process(target=run_app, args=(data,))
+    p1 = Process(target=run_app_server, args=(app,))
+
+    p.start()
+    p1.start()
+    
+    p1.join()
+    p.join()
+
