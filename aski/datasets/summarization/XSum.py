0ea840bdecfcdaaa5dd48f99f3bd3ec65cec8454
Thomas Le Menestrel <tlemenestrel@hotmail.fr>
2022-08-16 02:50:33 -0700

    Adding class for XSum dataset

---
+++ aski/datasets/summarization/XSum.py
@@ -0,0 +1,14 @@
+"""
+XSumDataset
+====================================================
+This module extends the Dataset Class and is used to load specifically from the 
+XSum Dataset
+"""
+
+from aski.datasets.interfaces.hugging_face_dataset import HuggingFaceDataset
+
+class XSum(HuggingFaceDataset):
+
+	def __init__(self):
+		super().__init__(dataset_name='xsum', config='3.0.0')
+		
